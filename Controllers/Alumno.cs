dc511253713ee9955c13618980e19beae689e069
mishel layza <layzavillacortae@iestptrujillo.net>
2024-09-19 11:01:34 -0500

    mi primer commit

---
+++ Controllers/Alumno.cs
@@ -0,0 +1,47 @@
+﻿namespace WebApi.Controllers
+{
+    public class Alumno
+    {
+        //creando propiedades
+        string Nombre;
+        string Apellido;
+        string dni;
+        DateTime fechanacimiento;
+
+        //constructor sin parametros
+        public Alumno()
+        {
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            dni = string.Empty;
+            fechanacimiento = DateTime.Now;
+        }
+        //constructor con parametros
+        public Alumno(string pNombre, string pApellido, string pdni)
+        {
+        Nombre = pNombre;
+        Apellido = pApellido;
+        dni = pdni;
+        }
+
+        public Alumno(string pNombre, string pApellido, string pdni, DateTime pfechanacimiento)
+        {
+            Nombre = pNombre;
+            Apellido = pApellido;
+            dni = pdni;
+            fechanacimiento = pfechanacimiento;
+        }
+
+
+        public string DevolverDocumento()
+        {
+            return dni;
+        }
+
+        public int DevolverEdad()
+        {
+            return DateTime.Now.Year - fechanacimiento.Year;
+        }
+
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("DevolverSuma")]
+        public string DevolverSuma()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno1 = new Alumno();
+            Alumno Alumno2 = new Alumno("Juan", "Perez", "4370258");
+            return Alumno2.DevolverDocumento();
+        }
+
+
+        [HttpGet("DevolverEdad")]
+        public int DevolverEdad()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno3 = new Alumno("Mishel", "Layza", "72785831", new DateTime(2006, 2, 2));
+            return Alumno3.DevolverEdad();
+        }
+
+
+
+    }
+}
